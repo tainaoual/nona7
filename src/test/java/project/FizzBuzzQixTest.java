a0ad51557cc9d0ff6aba0752170ba7d28e43c3e2
Benjamin Williams <DeanPBR@gmail.com>
2020-09-24 22:31:55 -0400

    Reformating code

--- src/test/java/project/FizzBuzzQixTest.java
+++ src/test/java/project/FizzBuzzQixTest.java
@@ -6,7 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class FizzBuzzQixTest {
     @Test
-    public void oneShouldReturnOne(){
+    public void oneShouldReturnOne() {
         FizzBuzzQix underTest = new FizzBuzzQix();
         String spokenWord = underTest.interpret(1);
         assertThat(spokenWord).isEqualTo("1");
