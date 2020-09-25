ba6175128b32f086d3c73767bf32232ec76ac351
Benjamin Williams <DeanPBR@gmail.com>
2020-09-24 22:21:59 -0400

    Adding stubs

---
+++ src/main/java/project/FizzBuzzQix.java
@@ -0,0 +1,7 @@
+package project;
+
+public class FizzBuzzQix {
+    public String interpret(int numberToInterpret) {
+        return null;
+    }
+}
---
+++ src/test/java/project/FizzBuzzQixTest.java
@@ -0,0 +1,14 @@
+package project;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FizzBuzzQixTest {
+    @Test
+    public void oneShouldReturnOne(){
+        FizzBuzzQix underTest = new FizzBuzzQix();
+        String spokenWord = underTest.interpret(1);
+        assertThat(spokenWord).isEqualTo("1");
+    }
+}
