79a0df10cf0bf5c03400515613b2fe4251fd6d78
tainaoual <Tai Naoual>
2020-09-26 23:36:04 -0400

    test

--- src/main/java/project/FizzBuzzQix.java
+++ src/main/java/project/FizzBuzzQix.java
@@ -1,7 +1,57 @@
 package project;
 
 public class FizzBuzzQix {
+    private String StringToInterpret;
+    String Fizz = "Fizz";
+    String Buzz = "Buzz";
+    String Qix = "Qix";
+    String FizzBuzzQix = "FizzBuzzQix";
+
+
+
     public String interpret(int numberToInterpret) {
+
+        if (numberToInterpret % 3 == 0) {
+
+            return "Fizz";
+
+        } else if (numberToInterpret % 5 == 0) {
+
+            return "Buzz";
+            //  }else{
+            //     return null;
+            // }
+        }else if (numberToInterpret % 7 == 0 ){
+            return "Qix";
+        }else if (numberToInterpret == 105){
+            return  "FizzBuzzQix";
+        }
         return null;
-    }
-}
+    }}
+//}num
+//berToInterpret % 3 &&numberToInterpret % 5 == 0 && numberToInterpret % 7 == 0
+
+
+
+//    Given This week we are changing things up, we are only giving you one test, you have to write the rest on your own. We are taking the old Fizz Buzz kata out for a ride, but we are adding a new feature to develop: Qix. The functionality you must test out for FizzBuzzQix is as follows:
+
+      //  Given an integer, return a String representation of that integer: 1 -> "1" and 2 -> "2" for example.
+      //  Given an integer that is a multiple of 3, return a String that contains the word "Fizz".
+     //   Given an integer that is a multiple of 5, return a String that contains the word "Buzz".
+      //  an integer that is a multiple of 7, return a String that contains the word "Qix".
+      //  If multiple words are used, add them in order of Fizz, Buzz, then Qix. For example: 105 -> FizzBuzzQix
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/test/java/project/FizzBuzzQixTest.java
+++ src/test/java/project/FizzBuzzQixTest.java
@@ -1,14 +1,43 @@
+
 package project;
 
-import org.junit.jupiter.api.Test;
+        import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+        import static org.assertj.core.api.Assertions.assertThat;
 
 public class FizzBuzzQixTest {
+    private Object StringToInterpret;
+
     @Test
     public void oneShouldReturnOne() {
         FizzBuzzQix underTest = new FizzBuzzQix();
         String spokenWord = underTest.interpret(1);
         assertThat(spokenWord).isEqualTo("1");
     }
+
+    @Test
+    public void multipleOfTreeShouldReturnFizz() {
+        FizzBuzzQix underTest = new FizzBuzzQix();
+        String spokenWord = underTest.interpret(3);
+        assertThat(spokenWord).isEqualTo("Fizz");
+    }
+    @Test
+    public void multipleOfFiveShouldReturnBuzz() {
+        FizzBuzzQix underTest = new FizzBuzzQix();
+        String spokenWord = underTest.interpret(5);
+        assertThat(spokenWord).isEqualTo("Buzz");
+    }
+    @Test
+    public void multipleOfSevenShouldReturnQix() {
+        FizzBuzzQix underTest = new FizzBuzzQix();
+        String spokenWord = underTest.interpret(7);
+        assertThat(spokenWord).isEqualTo("Qix");
+    }
+    @Test
+    public void multipleOfWordsAddShouldReturninorderFizzBzzQix() {
+        FizzBuzzQix underTest = new FizzBuzzQix();
+        String spokenWord = underTest.interpret(105);
+        assertThat(spokenWord).isEqualTo("FizzBuzzQix");
+    }
+
 }
